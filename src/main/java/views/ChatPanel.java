c2faf83370698638883c48f61ade3e344f755cc2
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-04-29 15:41:28 +0900

    Feat: Add chat pane

--- src/main/java/views/ChatPanel.java
+++ src/main/java/views/ChatPanel.java
@@ -1,7 +1,44 @@
 package views;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
 public class ChatPanel extends JPanel {
+
+  /**
+   * Chat rooms panel.
+   */
+  private JPanel chatRoomsPanel;
+
+  /**
+   * Chatroom list.
+   */
+  private JList<String> chatRooms;
+
+  public ChatPanel() {
+    setLayout(new BorderLayout());
+
+    chatRoomsPanel = new JPanel();
+    String[] sampleChatRooms = {
+        "협동분산시스템", "분산시스템및컴퓨팅", "소프트웨어V&V", "클라우드IOT서비스", "실전취업특강", "HCI",
+        "협동분산시스템", "분산시스템및컴퓨팅", "소프트웨어V&V", "클라우드IOT서비스", "실전취업특강", "HCI",
+        "협동분산시스템", "분산시스템및컴퓨팅", "소프트웨어V&V", "클라우드IOT서비스", "실전취업특강", "HCI",
+        "협동분산시스템", "분산시스템및컴퓨팅", "소프트웨어V&V", "클라우드IOT서비스", "실전취업특강", "HCI"
+    };
+    chatRooms = new JList<>(sampleChatRooms);
+    chatRooms.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    chatRooms.setFont(new Font("Nanum Gothic", Font.PLAIN, 28));
+    JScrollPane scrollPane = new JScrollPane(chatRooms, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    chatRoomsPanel.add(scrollPane);
+    scrollPane.setPreferredSize(new Dimension(580, 500));
+
+    add(chatRoomsPanel, BorderLayout.WEST);
+  }
 
 }
