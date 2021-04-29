38372709080951791b8abb7837c1681175cf7ce2
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-04-29 15:27:07 +0900

    Update profile pane

--- src/main/java/views/ProfilePanel.java
+++ src/main/java/views/ProfilePanel.java
@@ -3,7 +3,6 @@ package views;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -11,8 +10,6 @@ import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
 
 public class ProfilePanel extends JPanel {
-
-  private JScrollPane scrollPane;
 
   /**
    * Owner profile.
@@ -33,16 +30,24 @@ public class ProfilePanel extends JPanel {
     setLayout(new BorderLayout());
 
     ownerInfoTextArea = new JTextArea();
-    ownerInfoTextArea.setFont(new Font("Nanum Gothic", Font.BOLD, 18));
+    ownerInfoTextArea.setFont(new Font("Nanum Gothic", Font.BOLD, 20));
     ownerInfoTextArea.setText("신윤섭");
 
     othersPanel = new JPanel();
-    String[] sampleUsers = {"유경원", "유진욱", "한지희", "임민규"};
+    String[] sampleUsers = {
+        "유경원", "유진욱", "한지희", "임민규", "유경원", "유진욱", "한지희", "임민규", "유경원", "유진욱",
+        "한지희", "임민규", "유경원", "유진욱", "한지희", "임민규", "유경원", "유진욱", "한지희", "임민규",
+        "유경원", "유진욱", "한지희", "임민규", "유경원", "유진욱", "한지희", "임민규", "유경원", "유진욱"
+    };
     others = new JList<>(sampleUsers);
     others.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-    othersPanel.add(others);
+    others.setFont(new Font("Nanum Gothic", Font.PLAIN, 16));
+    JScrollPane scrollPane = new JScrollPane(others, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+        JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    othersPanel.add(scrollPane);
+    scrollPane.setPreferredSize(new Dimension(580, 480));
 
     add(ownerInfoTextArea, BorderLayout.NORTH);
-    add(othersPanel, BorderLayout.CENTER);
+    add(othersPanel, BorderLayout.WEST);
   }
 }
