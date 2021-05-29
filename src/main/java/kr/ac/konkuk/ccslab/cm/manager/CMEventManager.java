cbbc13c86345c20ef310445b5180544bc5218051
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-05-29 23:02:53 +0900

    Fix: Replace peer-server logic to client-server logic

--- src/main/java/kr/ac/konkuk/ccslab/cm/manager/CMEventManager.java
+++ src/main/java/kr/ac/konkuk/ccslab/cm/manager/CMEventManager.java
@@ -183,6 +183,7 @@ public class CMEventManager {
 	
 	public synchronized static boolean unicastEvent(CMEvent cme, String strReceiver, CMInfo cmInfo)
 	{
+		System.out.println("unicastEvent");
 		return unicastEvent(cme, strReceiver, CMInfo.CM_STREAM, 0, false, cmInfo);
 	}
 	
